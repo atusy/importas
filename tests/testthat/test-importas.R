6a4cf6e517148a5dfc5c227b9f2141c6e3e4c774
atusy <30277794+atusy@users.noreply.github.com>
2019-12-22 21:33:01 +0900

    update tests

--- tests/testthat/test-importas.R
+++ tests/testthat/test-importas.R
@@ -1,7 +1,5 @@
 test_that("Define aliases at once", {
   importas(gr = graphics, st = stats)
-  expect_identical(gr, getNamespace("graphics"))
   expect_identical(gr$plot, graphics::plot)
-  expect_identical(st, getNamespace("stats"))
   expect_identical(st$median, stats::median)
 })
