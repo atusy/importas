25cbad8220acbfae68d2cad1034a1f572e99a3ad
atusy <30277794+atusy@users.noreply.github.com>
2020-09-04 22:39:35 +0900

    improve performance

--- R/construct-importas.R
+++ R/construct-importas.R
@@ -1,11 +1,11 @@
 #' Construct an importas class object
 #'
-#' @param pkg A package's name
+#' @param pkg A package's name as a string
 #' @noRd
 construct_importas <- function(pkg) {
   exports <- getNamespaceExports(pkg)
   structure(
-    as.list(exports),
+    vector("list", length(exports)),
     names = exports,
     class = c("importas", "list"),
     package = pkg
