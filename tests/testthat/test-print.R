69ac5b7a63b8507c0dbefb733906e3ccec34793b
atusy <30277794+atusy@users.noreply.github.com>
2020-09-04 23:22:58 +0900

    update test

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -1,6 +1,7 @@
 test_that("Print exported objects", {
+  stats %as% st
   expect_identical(
-    capture.output(print(stats %as% st)),
+    capture.output(print(st)),
     capture.output(print(sort(getNamespaceExports("stats"))))
   )
 })
