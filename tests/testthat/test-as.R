3489a6217ceddbfc54fd94002a9d9a24db2f9852
Atsushi Yasumoto <atsushi@hacarus.com>
2020-09-08 08:48:08 +0900

    add tests

--- tests/testthat/test-as.R
+++ tests/testthat/test-as.R
@@ -1,4 +1,9 @@
 test_that("Abbreviation works", {
   expect_null(graphics %as% gr)
   expect_identical(gr$plot, graphics::plot)
+  if ("MASS" %in% rownames(installed.packages())) {
+    expect_false("package:MASS" %in% search())
+    expect_null(MASS %as% mass)
+    expect_identical(mass$area, MASS::area)
+  }
 })
